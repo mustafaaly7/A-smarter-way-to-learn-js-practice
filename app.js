b51809c8b65a37b4aef9d01ad5ab8575bae2e08d
Mustafa Ali <mustafaaaly27@gmail.com>
2024-05-08 19:17:59 +0500

    chapter 2

---
+++ app.js
@@ -0,0 +1,43 @@
+// JS CHAPTER 1
+// alert("HELLO VISITORS")
+// alert("Error !  Please Enter valid Password");
+// alert("Welcome to Js Land \n Happy Coding :" )
+
+// alert("Welcome to js Land")
+// alert(" I can Run Js through my web browser")
+// alert("Happy coding")
+
+
+
+
+// JS Chapter 2
+
+// var userName ; 
+// var myName = "Muhammad Mustafa Ali"
+
+// var message = "hello World "
+// alert(message);
+
+
+// var fullName = prompt("Enter your Full Name ")
+// var  age = +prompt("Enter Your age")
+// var qualification = prompt("Enter Your current Qualificaation")
+// alert(fullName)
+// alert(age)
+// alert(qualification)
+
+
+
+// var data = "PIZZA"
+// alert(data + "\n" + data.slice(0,-1) + "\n" + data.slice(0,-2) +"\n" + data.slice(0,-3) +"\n" + data.slice(0,-4) +"\n" + data.slice(0,-5))
+
+
+
+// var userEmail = prompt("Enter your Email");
+// alert(userEmail + "@gmail.com")
+
+
+// var book ="smarter way to learn JavaScript"
+// alert(book)
+
+
