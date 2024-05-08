991b2e45583b6c30f490b70be2fb0ffa5607de76
Mustafa Ali <mustafaaaly27@gmail.com>
2024-05-08 20:37:58 +0500

    chapter 3 completed

--- app.js
+++ app.js
@@ -50,6 +50,55 @@
 
 // JS CHAPTER 3
 
+// QUESTION: (VERY IMPORTANT ) Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. 
+
+
+// var visitCounter = localStorage.getItem("Enteries"); // generate key and won't change even if the tab is closed
+
+
+// if (visitCounter === null){
+
+
+//     visitCounter = 0;
+// }
+// visitCounter++;
+// localStorage.setItem("Enteries" , visitCounter);
+
+
+// document.write("you've visited this site " + visitCounter)
+
+// Different Question
+// var birthYear = 2003;
+// document.write("My Birthyear is "+ birthYear +"<br>");
+// document.write("Data type of my declared variable is "+ typeof birthYear);
+
+
+
+// var userName = "john "
+// var product = "Tshirts"
+// var quantity ="5"
+// document.write(userName + " ordered" + quantity +product)
+
+// ----- > JS chapter 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
